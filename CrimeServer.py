a02c2578bea89275eb07d72ebab809a61e7e432b
Gabe <angelgabe101@gmail.com>
2019-02-23 13:39:55 -0800

    Update CrimeServer.py

--- CrimeServer.py
+++ CrimeServer.py
@@ -1,13 +1,13 @@
 from flask import Flask
 import requests
-import YelpAPI
+import recieveJSON
 import json
 
 CrimeServer = Flask(__name__)
 
 @CrimeServer.route('/')
 def apiRequest():
-    y = json.load(YelpAPI.getRestaurants("icecream", -74.7771916 , 40.272249099999996, 5))
-    return y.text()
+    y = recieveJSON.getRestaurants("icecream", -74.7771916 , 40.272249099999996, 5)['businesses'][0]['name']
+    return y
 if __name__ == '__main__':
     CrimeServer.run() 
